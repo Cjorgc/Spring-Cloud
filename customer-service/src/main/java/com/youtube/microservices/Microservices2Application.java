8d228d6a69a1f1fcd12e0cec239f7e2b7d3ade48
Jorge Condori <jcondori@baufest.com>
2021-08-17 16:09:47 -0300

    Added all eureka clients

--- customer-service/src/main/java/com/youtube/microservices/Microservices2Application.java
+++ customer-service/src/main/java/com/youtube/microservices/Microservices2Application.java
@@ -2,8 +2,10 @@ package com.youtube.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Microservices2Application {
 
 	public static void main(String[] args) {
