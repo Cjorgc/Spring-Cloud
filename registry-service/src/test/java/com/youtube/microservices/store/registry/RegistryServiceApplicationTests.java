5aa2dfb07808bbd2f20312447238ec2358c31c71
Jorge Condori <jcondori@baufest.com>
2021-08-17 15:22:01 -0300

    add eureka-registry-service

---
+++ registry-service/src/test/java/com/youtube/microservices/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.microservices.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
